66606bdcdfb0cac63daa6f230acf133a0c5be64f
yujin Park <101633842+ruby-yujin@users.noreply.github.com>
2024-04-16 22:26:58 +0900

    [feature] router setting /  Nav to change compoents name Header

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Mainpage from "./main/Mainpage";
+import Detail from "./detail/Detail";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Mainpage />} />
+        <Route path="/detail" element={<Detail />} />
+        {/* <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
