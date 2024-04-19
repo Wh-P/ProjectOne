e252a78f55fa91ff863020d77db0f614a76eb9e2
woohyuk Park <psj4415@naver.com>
2024-04-19 10:31:23 +0900

    [feature]Draft기능추가

--- src/register/components/Register.tsx
+++ src/register/components/Register.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import SelectComponent from "./Select";
 import Datepicker from "./Datepicker";
+import MyEditor from "./Draft";
 
 const Register = () => {
   const options = [
@@ -53,14 +54,13 @@ const Register = () => {
           title={"스터디"}
           label={"모집 포지션"}
         />
-        
-          <SelectComponent
-            options={options}
-            title={"스터디"}
-            label={"연락 방법"}
-          />
-          <StyledInput type="text" placeholder="오픈 카톡방 링크" />
-        
+
+        <SelectComponent
+          options={options}
+          title={"스터디"}
+          label={"연락 방법"}
+        />
+        <StyledInput type="text" placeholder="오픈 카톡방 링크" />
       </RegisterDivs>
 
       <RegisterSectionTwo>
@@ -69,10 +69,18 @@ const Register = () => {
       </RegisterSectionTwo>
       <DraftSection>
         <DraftSectiondiv>
-        <DraftSectionLabel>제목</DraftSectionLabel>
-        <DraftSectionInput type="text" placeholder="글 제목을 입력해주세요!"></DraftSectionInput>
+          <DraftSectionLabel>제목</DraftSectionLabel>
+          <DraftSectionInput
+            type="text"
+            placeholder="글 제목을 입력해주세요!"
+          ></DraftSectionInput>
         </DraftSectiondiv>
       </DraftSection>
+      <MyEditor />
+      <DraftButtonsDiv>
+        <DraftButtonCancel>취소</DraftButtonCancel>
+        <DraftButtonWrite>글 등록</DraftButtonWrite>
+      </DraftButtonsDiv>
     </RegisterDiv>
   );
 };
@@ -93,7 +101,7 @@ const RegisterSectionOne = styled.div`
   align-items: center;
   flex-direction: row;
   padding: 16px;
-  margin-top:10px;
+  margin-top: 10px;
   margin-bottom: 36px;
   border-bottom: 2.4px solid rgb(242, 242, 242);
 `;
@@ -122,12 +130,11 @@ const RegisterDivs = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
-  margin-top: 40px;
+ 
   box-sizing: border-box;
   @media (max-width: 768px) {
     flex-direction: column;
-    margin:20px;
-
+    margin: 20px;
   }
 `;
 const RegisterSectionTwo = styled.div`
@@ -143,7 +150,7 @@ const RegisterSectionTwo = styled.div`
 const StyledInput = styled.input`
   width: 49%;
   height: 60px;
-  margin-top:20px;
+  margin-top: 20px;
   margin-left: auto;
   padding: 2px 8px;
   border: 1px solid rgb(224, 224, 224);
@@ -157,26 +164,59 @@ const DraftSection = styled.section`
   display: flex;
   align-items: start;
   flex-direction: column;
-  .dDXAvR{
-    margin:20px;
+  .dDXAvR {
+    margin: 20px;
   }
 `;
 
 const DraftSectiondiv = styled.div`
-  margin:20px;
-  width:94%;
+  margin-bottom: 10px;
+  margin-top: 10px;
+  width: 100%;
 `;
 
 const DraftSectionLabel = styled.label`
-  font-weight:bold;
-  margin-bottom:5px;
+  font-weight: bold;
+  margin-bottom: 5px;
 `;
 
-const DraftSectionInput =styled.input`
-  width:100%;
-  height:56px;
-  margin:10px 8px 0px 0px;
+const DraftSectionInput = styled.input`
+  width: 100%;
+  height: 56px;
+  margin: 10px 8px 0px 0px;
   padding: 0px 52px 0px 16px;
   border: 1px solid rgb(224, 224, 224);
-  border-radius:5px;
+  border-radius: 5px;
+`;
+const DraftButtonsDiv = styled.section`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin-top: 1rem;
+`;
+
+const DraftButtonCancel = styled.button`
+    cursor: pointer;
+    outline: none;
+    border: none;
+    border-radius: 4px;
+    padding: 0 1.25rem;
+    height: 2rem;
+    font-size: 1rem;
+    background: #e9ecef;
+    color: #495057;
+    margin-right: 1rem;
+`
+const DraftButtonWrite = styled.button`
+  cursor: pointer;
+    outline: none;
+    border: none;
+    border-radius: 4px;
+    padding: 0 1.25rem;
+    height: 2rem;
+    font-size: 1rem;
+    font-weight: 700;
+    background-color: #262626;
+    color: #fff;
+    margin-right: 1rem;
 `
